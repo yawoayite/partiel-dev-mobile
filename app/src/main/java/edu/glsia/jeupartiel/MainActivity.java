699965e2fe2f4829b85792c57192a8f93bf70b37
Flo_Ayt <39480651+FlorianAyt@users.noreply.github.com>
2021-06-10 14:43:22 +0000

    Implementing registration,shared preferences and menu

---
+++ app/src/main/java/edu/glsia/jeupartiel/MainActivity.java
@@ -0,0 +1,72 @@
+package edu.glsia.jeupartiel;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonAccueil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater  =  getMenuInflater();
+        inflater.inflate(R.menu.menu_accueil,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuButtonScore:
+                goToScorePage();
+                return true;
+            case R.id.menuButtonJouer:
+                goToGamePage();
+                return true;
+            case R.id.meunButtonEnregistrer:
+                goToRegistrerPage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void goToScorePage(){
+        Intent  scoreIntent = new Intent(getBaseContext(),ScoreActivity.class);
+        startActivity(scoreIntent);
+    }
+
+    public void goToGamePage(){
+        Intent gameIntent = new Intent(getBaseContext(),GameActivity.class);
+        startActivity(gameIntent);
+    }
+
+    public void goToRegistrerPage(){
+        Intent registerIntent = new Intent(getBaseContext(),RegisterActivity.class);
+        startActivity(registerIntent);
+    }
+
+
+}
---
+++ app/src/main/java/edu/glsia/jeupartiel/RegisterActivity.java
@@ -0,0 +1,84 @@
+package edu.glsia.jeupartiel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText nomEditText,pseudoEditext;
+    Button btEnregistrer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        nomEditText = findViewById(R.id.nomEditText);
+        pseudoEditext = findViewById(R.id.pseudoEditText);
+        btEnregistrer = findViewById(R.id.btEnregistrer);
+
+        applyPreferences();
+
+        btEnregistrer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register(nomEditText,pseudoEditext);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public void saveNomPreferences(EditText editText){
+        SharedPreferences nomSharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor  editor = nomSharedPref.edit();
+        editor.putString("nom",editText.getText().toString());
+        editor.apply();
+    }
+
+    public void savePseudoPreferences(EditText editText){
+        SharedPreferences pseudoSharedPref =  getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pseudoSharedPref.edit();
+        editor.putString("pseudo",editText.getText().toString());
+        editor.apply();
+    }
+
+
+    public void applyPreferences(){
+        SharedPreferences pseudoSharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        String lcp = pseudoSharedPreferences.getString("pseudo","floflo");
+        if(!lcp.equals("floflo")){
+            pseudoEditext.setText(String.valueOf(lcp));
+        }
+
+        SharedPreferences nomSharedPref = getPreferences(Context.MODE_PRIVATE);
+        String lecompteur =  nomSharedPref.getString("nom","flo");
+        if(!lecompteur.equals("flo")){
+            nomEditText.setText(String.valueOf(lecompteur));
+        }
+
+    }
+
+    public void register(EditText nomEditText,EditText pseudoEditext){
+        String nomText = (nomEditText.getText()).toString();
+        String pseudoText = (pseudoEditext.getText()).toString();
+
+        if(nomText == null){
+            Toast.makeText(getApplicationContext(),"Le champ nom est vide",Toast.LENGTH_LONG).show();
+        }else if (pseudoText == null){
+            Toast.makeText(getApplicationContext(),"Le champ pseudo est vide",Toast.LENGTH_LONG).show();
+        }else {
+            saveNomPreferences(nomEditText);
+            savePseudoPreferences(pseudoEditext);
+        }
+    }
+}
